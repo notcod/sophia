5b37803beaa087fc8ad9f3b6e5b303a227479220
Nemanja M <mneca99+git@gmail.com>
2021-01-16 06:39:33 +0100

    updated

--- config/config.php
+++ config/config.php
@@ -1,12 +1,6 @@
 <?php
 session_start();
 
-//Database params
-define('DB_HOST', 'localhost'); //Add your db host
-define('DB_USER', 'root'); // Add your DB root
-define('DB_PASS', ''); //Add your DB pass
-define('DB_NAME', 'proto'); //Add your DB Name
-
 //APPROOT
 define('APPROOT', dirname(dirname(dirname(__FILE__))));
 
@@ -14,17 +8,27 @@ $root = str_replace($_SERVER["DOCUMENT_ROOT"], "", str_replace("\\", "/", dirnam
 define('ROOT', $root.'/');
 define('IMGROOT', $root == '/' ? '' : substr(ROOT,0,-1));
 
+
+############################################
+    /** CHANGE INFORMATIONS BELLOW */
+############################################
+
+
 //URLROOT (Dynamic links)
 define('URLROOT', 'https://milioner.com'.$root);
 
 define('SITENAME', 'Sophia PHP framework');
 
+//Database params
+define('DB_HOST', 'localhost'); //Add your db host
+define('DB_USER', 'root'); // Add your DB root
+define('DB_PASS', ''); //Add your DB pass
+define('DB_NAME', 'test'); //Add your DB Name
 
 define("EMAIL_NAME", "Sophia");
-define("EMAIL_HOST", "server.example.rs");
-define("EMAIL_USERNAME", "122121@example.com");
-define("EMAIL_PASSWORD", "12221212");
+define("EMAIL_HOST", "smtp.google.com");
+define("EMAIL_USERNAME", "example@gmail.com");
+define("EMAIL_PASSWORD", "undefiend");
 
 
 define("DEV_MODE", "1");
-define("MINIMIZE_CSS", "0");
