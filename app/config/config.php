7417405cb37a5f088cf274e8fe469ab37be61f91
Nemanja M <mneca99+git@gmail.com>
2021-01-17 03:04:19 +0100

    updated

---
+++ app/config/config.php
@@ -0,0 +1,17 @@
+<?php
+############################################
+    /** CHANGE INFORMATIONS BELLOW */
+############################################
+
+define('SITENAME', 'Sophia PHP framework');
+
+//Database params
+define('DB_HOST', 'localhost'); //Add your db host
+define('DB_USER', 'root'); // Add your DB root
+define('DB_PASS', ''); //Add your DB pass
+define('DB_NAME', 'test'); //Add your DB Name
+
+define("EMAIL_NAME", "Sophia");
+define("EMAIL_HOST", "smtp.google.com");
+define("EMAIL_USERNAME", "example@gmail.com");
+define("EMAIL_PASSWORD", "undefiend");
---
+++ app/views/docs.php
@@ -0,0 +1,22 @@
+<div class="d-flex" id="wrapper">
+  <div id="sidebar-wrapper">
+    <div class="list-group list-group-flush">
+      <a href="<?=ROOT?>docs/index" class="list-group-item main-sidebar"><i class="fas fa-tachometer-alt"></i> Overview</a>
+      <a href="<?=ROOT?>docs/Controllers" class="list-group-item main-sidebar"><i class="fas fa-briefcase"></i> Controllers</a>
+      <a href="<?=ROOT?>docs/Models" class="list-group-item main-sidebar"><i class="fas fa-users-cog"></i> Models</a>
+      <a href="<?=ROOT?>docs/Views" class="list-group-item main-sidebar"><i class="far fa-list-alt"></i> Views</a>
+      <a href="<?=ROOT?>docs/Requests" class="list-group-item main-sidebar"><i class="fas fa-list-ol"></i> Requests</a>
+      <a href="<?=ROOT?>docs/Template" class="list-group-item main-sidebar"><i class="fas fa-object-ungroup"></i> Template</a>
+      <a href="<?=ROOT?>docs/Support" class="list-group-item main-sidebar"><i class="fas fa-cogs"></i> Support</a>
+    </div>
+  </div>
+  <nav class="navbar navbar-expand-lg navbar-light fixed-top bg-blue text-white" style="box-shadow: 0 2px 5px 0 rgba(0,0,0,0.16), 0 2px 10px 0 rgba(0,0,0,0.12);">
+    <button class="btn text-white" id="menu-toggle"><i class="fa fa-bars" aria-hidden="true"></i></button>
+    <div class="sidebar-heading mr-auto font-weight-bold">SOPHIA</div>
+  </nav>
+  <div id="page-content-wrapper">
+    <div class="container-fluid py-3" style="margin-top:55px;">
+      <?php content("$data[view]/$data[page].php", $data); ?>
+    </div>
+  </div>
+</div>
